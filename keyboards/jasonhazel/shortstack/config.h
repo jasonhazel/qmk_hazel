f0e7ceb2456b319e3fda16288c499576661d0d40
Jason Hazel <jhazel@matrixmediaservices.com>
2023-03-05 15:39:15 -0500

    fix matrix row and col order

--- keyboards/jasonhazel/shortstack/config.h
+++ keyboards/jasonhazel/shortstack/config.h
@@ -5,8 +5,8 @@
 #define MATRIX_ROWS 8
 #define MATRIX_COLS 6
 
-#define MATRIX_ROW_PINS { B6, B2, B3, B1 }
-#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS { B1, B3, B2, B6 }
+#define MATRIX_COL_PINS { B5, B4, E6, D7, C6, D4 }
 
 // #define MATRIX_ROW_PINS_RIGHT { D7, E6, B4, B5 }
 // #define MATRIX_COL_PINS_RIGHT { B2, B3, B1, F7, F6 }
